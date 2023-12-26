7da40a9cf42624c28bf1a3864be985afce2583f6
jinyr1128 <jinyr1128@gmail.com>
2023-12-26 11:20:15 +0900

    create

---
+++ src/main/java/com/ddproject/DdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ddproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddproject/DdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
