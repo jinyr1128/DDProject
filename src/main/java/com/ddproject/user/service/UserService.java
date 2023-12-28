f63148f94b2ce363fcc10507969a22ea74c28d70
Gwon JunHyeok <jh10253267@gmail.com>
2023-12-29 02:01:52 +0900

    feat(User): 기능구현 마무리
    기능구현과 함께 서비스 인터페이스 수정

--- src/main/java/com/ddproject/user/service/UserService.java
+++ src/main/java/com/ddproject/user/service/UserService.java
@@ -1,10 +1,11 @@
 package com.ddproject.user.service;
 import com.ddproject.user.dto.CheckRequestDto;
 import com.ddproject.user.dto.PasswordDto;
+import com.ddproject.user.dto.SignupResponseDto;
 import com.ddproject.user.dto.SignupUserDto;
 
 public interface UserService {
-    SignupUserDto signup(String username, String email, String password);
+    SignupResponseDto signup(SignupUserDto signupUserDto);
     boolean validateSignup(CheckRequestDto checkRequestDto);
 
     void changePw(PasswordDto passwordDto, String username);
