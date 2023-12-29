c396d7989a14a020832cbf91801af40aa41a70f3
jinyr1128 <jinyr1128@gmail.com>
2023-12-29 15:21:42 +0900

    feat(Column) : Lombok 사용

--- src/main/java/com/ddproject/column/entity/Column.java
+++ src/main/java/com/ddproject/column/entity/Column.java
@@ -26,45 +26,4 @@ public class Column {
     // 생성자
     public Column() {
     }
-
-    // getter와 setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(Integer sequence) {
-        this.sequence = sequence;
-    }
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
-    }
 }
