5a7d855ff300e990eb2df2395b9ad84278130d65
jinyr1128 <jinyr1128@gmail.com>
2023-12-29 10:58:09 +0900

    feat(Column) : 어노테이션 추가

--- src/main/java/com/ddproject/column/entity/Column.java
+++ src/main/java/com/ddproject/column/entity/Column.java
@@ -2,7 +2,11 @@ package com.ddproject.column.entity;
 
 import com.ddproject.board.entity.Board;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "columns")
 public class Column {
