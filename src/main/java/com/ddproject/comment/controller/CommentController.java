cb6cb3c926ec67b2c3de42297d20bfbc156762fc
jinyr1128 <jinyr1128@gmail.com>
2023-12-29 19:20:08 +0900

    feat(Comment) : CommentController

--- src/main/java/com/ddproject/comment/controller/CommentController.java
+++ src/main/java/com/ddproject/comment/controller/CommentController.java
@@ -1,4 +1,24 @@
 package com.ddproject.comment.controller;
 
+import com.ddproject.comment.dto.CommentDto;
+import com.ddproject.comment.service.CommentService;
+import com.ddproject.user.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/boards/{boardId}/columns/{columnId}/cards/{cardId}/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
 }
