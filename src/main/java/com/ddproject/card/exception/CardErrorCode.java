77f7cfabbdb0334c56b1f3f297aee6814cb30ef9
jinyr1128 <jinyr1128@gmail.com>
2023-12-31 19:38:54 +0900

    feat(Card) : HttpStatus

--- src/main/java/com/ddproject/card/exception/CardErrorCode.java
+++ src/main/java/com/ddproject/card/exception/CardErrorCode.java
@@ -1,25 +1,26 @@
 package com.ddproject.card.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CardErrorCode {
-    BOARD_NOT_FOUND(404, "보드를 찾을 수 없습니다."),
-    COLUMN_NOT_FOUND(404, "컬럼을 찾을 수 없습니다."),
-    CARD_NOT_FOUND(404, "카드를 찾을 수 없습니다."),
-    INVALID_CARD_DATA(400, "잘못된 카드 데이터입니다."),
-    CARD_UPDATE_FAILED(500, "카드 업데이트에 실패했습니다."),
-    CARD_DELETE_FAILED(500, "카드 삭제에 실패했습니다."),
-    CARD_CREATION_FAILED(500, "카드 생성에 실패했습니다."),
-    UNAUTHORIZED_CARD_ACCESS(401, "카드 접근 권한이 없습니다."),
-    FORBIDDEN_CARD_ACCESS(403, "카드 접근이 금지되었습니다."),
-    INTERNAL_CARD_SERVER_ERROR(500, "카드 관련 서버 내부 오류가 발생했습니다.");
+    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "보드를 찾을 수 없습니다."),
+    COLUMN_NOT_FOUND(HttpStatus.NOT_FOUND, "컬럼을 찾을 수 없습니다."),
+    CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "카드를 찾을 수 없습니다."),
+    INVALID_CARD_DATA(HttpStatus.BAD_REQUEST, "잘못된 카드 데이터입니다."),
+    CARD_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "카드 업데이트에 실패했습니다."),
+    CARD_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "카드 삭제에 실패했습니다."),
+    CARD_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "카드 생성에 실패했습니다."),
+    UNAUTHORIZED_CARD_ACCESS(HttpStatus.UNAUTHORIZED, "카드 접근 권한이 없습니다."),
+    FORBIDDEN_CARD_ACCESS(HttpStatus.FORBIDDEN, "카드 접근이 금지되었습니다."),
+    INTERNAL_CARD_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카드 관련 서버 내부 오류가 발생했습니다.");
 
-    private final int statusCode;
+    private final HttpStatus status;
     private final String message;
 
-    CardErrorCode(int statusCode, String message) {
-        this.statusCode = statusCode;
+    CardErrorCode(HttpStatus status, String message) {
+        this.status = status;
         this.message = message;
     }
 }
