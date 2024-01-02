a53a302bdb4b66c3858e004c891dd2c60948130c
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-02 17:55:17 +0900

    feat(User): 컨트롤러 테스트 작성 시작
    컨트롤러테스트 작성시작. 문제발생. 스웨거상에서는 회원가입경로에 대해 인증을 거치지 않는데 테스트 환경에서는 401상태코드를 반환하고있다. 이런 이유로 원인을 알아낼때 까지만 @WithMockUser를 사용할 예정이다.

--- src/test/java/com/ddproject/user/controller/UserControllerTest.java
+++ src/test/java/com/ddproject/user/controller/UserControllerTest.java
@@ -1,23 +1,32 @@
 package com.ddproject.user.controller;
 
+import com.ddproject.alarm.service.AlarmService;
+import com.ddproject.user.domain.User;
+import com.ddproject.user.dto.SignupResponseDto;
 import com.ddproject.user.dto.SignupUserDto;
+import com.ddproject.user.repository.UserRepository;
 import com.ddproject.user.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,20 +41,28 @@ class UserControllerTest {
     private ObjectMapper objectMapper;
     @MockBean
     private UserService userService;
+    @MockBean
+    private AlarmService alarmService;
 
     @Test
+    @WithMockUser
     void 회원가입_성공() throws Exception {
         String username = "username";
-        String email = "email";
+        String email = "email@test.com";
         String password = "password";
+        SignupUserDto signupUserDto = SignupUserDto.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
 
-//        when(userService.signup(username, email, password)).thenReturn(mock(SignupUserDto.class));
-//
-//        mvc.perform(post("/api/v1/users/signup")
-//                .with(csrf())
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(objectMapper.writeValueAsBytes(new SignupUserDto("username", "nekdoly123@naver.com", " password1234")))
-//        ).andDo(print())
-//                .andExpect(status().isOk());
+        when(userService.signup(signupUserDto)).thenReturn(mock(SignupResponseDto.class));
+
+        mvc.perform(post("/api/v1/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsBytes(signupUserDto))
+        ).andDo(print())
+                .andExpect(status().isOk());
     }
 }
