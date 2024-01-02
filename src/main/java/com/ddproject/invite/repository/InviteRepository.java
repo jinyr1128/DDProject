a0e95ac53bc880756eb7611a4fa1aadec77340f2
jinyr1128 <jinyr1128@gmail.com>
2024-01-02 10:24:20 +0900

    feat(Column) : update

--- src/main/java/com/ddproject/invite/repository/InviteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ddproject.invite.repository;
-
-
-import com.ddproject.invite.entity.Invite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InviteRepository extends JpaRepository<Invite, Long> {
-}
--- src/main/java/com/ddproject/invite/service/InviteService.java
+++
@@ -1,8 +0,0 @@
-package com.ddproject.invite.service;
-
-import com.ddproject.invite.dto.InviteDto;
-
-public interface InviteService {
-    void submitInvite(InviteDto inviteDto);
-    InviteDto readInvite(String username);
-}
