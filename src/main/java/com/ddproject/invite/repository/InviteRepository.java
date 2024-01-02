b3a820adc9c9e807693f23a34451c3db19ecf6df
Gwon JunHyeok <jh10253267@gmail.com>
2024-01-02 21:14:07 +0900

    feat(Invite): 유저가 받은 초대 목록을 확인하기 위한 레포지토리 메소드 추가
    유저가 받은 초대목록을 확인하기위한 쿼리메소드 추가.

--- src/main/java/com/ddproject/invite/repository/InviteRepository.java
+++ src/main/java/com/ddproject/invite/repository/InviteRepository.java
@@ -1,8 +1,12 @@
 package com.ddproject.invite.repository;
 
 
+import com.ddproject.invite.dto.InviteDto;
 import com.ddproject.invite.entity.Invite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InviteRepository extends JpaRepository<Invite, Long> {
+    List<Invite> findAllByRecvUsername(String username);
 }
