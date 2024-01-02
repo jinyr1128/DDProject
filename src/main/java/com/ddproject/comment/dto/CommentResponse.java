d1f2dddd68920409b0ddb28359671b5bb431c8e5
jinyr1128 <jinyr1128@gmail.com>
2024-01-02 18:22:50 +0900

    feat(comment) : commentResponse,commentRequest생성 후 처리 작업

--- src/main/java/com/ddproject/comment/dto/CommentResponse.java
+++ src/main/java/com/ddproject/comment/dto/CommentResponse.java
@@ -2,13 +2,12 @@ package com.ddproject.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import java.time.ZonedDateTime;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class CommentDto {
+public class CommentResponse {
     private Long id;
     private String text;
     private Long authorId;
